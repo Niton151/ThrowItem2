5e515d6847d919e83b3592d80afdd2c4bdcd1888
Nito <niton984@gmail.com>
2021-10-28 19:55:12 +0900

    拠点、クラフトスペース間のテレポーター設置

---
+++ Assets/Scripts/CraftSpaceTeleport.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CraftSpaceTeleport : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject player;
+
+    [SerializeField]
+    private Transform craftPos;
+
+    [SerializeField]
+    private Transform basePos;
+
+    [SerializeField]
+    private bool BaseToCraft;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(/*OVRInput.GetDown(OVRInput.RawButton.A) && */other.gameObject.CompareTag("Hand"))
+        {
+            if (BaseToCraft)
+            {
+                player.transform.position = craftPos.position;
+            }
+            else
+            {
+                player.transform.position = basePos.position;
+            }
+        }
+    }
+}
